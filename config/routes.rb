2bd42cb1bd2db8f55dba7a6e4c2c0237378cb040
Meghana Singh <meghana1602@gmail.com>
2017-02-28 17:09:02 +0000

    Completed checkpoint-23

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  resources :users, only: [:new, :create]
+  
   resources :topics do
     resources :posts, except: [:index]
     resources :sponsored_posts, except: [:index]
